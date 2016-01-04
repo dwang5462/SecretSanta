b06c507b3d2c13ad15536888b649833bcd008627
dwang5462 <dwang5462@yahoo.com>
2016-01-03 20:08:44 -0800

    fixed merge changes

--- SecretSanta.h
+++ SecretSanta.h
@@ -2,45 +2,29 @@
 #define SECRETSANTA_H
 
 #include <iostream>
-<<<<<<< HEAD
 #include <string>
 //#include <math>
-=======
 #include <math>
 #include <string>
->>>>>>> origin/master
 #include <random>
 
 class SecretSanta {
 public:
-<<<<<<< HEAD
 	SecretSanta();
 	SecretSanta(std::string name);
-=======
->>>>>>> origin/master
 	SecretSanta(std::string name, std::string wishList);
 	std::string getName() const;
 	std::string getWishList() const;
 	void addToWishList(std::string item);
 	void removeFromWishList(std::string item);
-<<<<<<< HEAD
-	std::string getRecipient() const;
-	std::string getRecipientWishList() const;
-=======
 	SecretSanta getRecipient() const;
 	std::string getRecipientWishlist() const;
->>>>>>> origin/master
 
 private:
 	std::string name;
 	std::string wishList;
 	SecretSanta* recipient;
-<<<<<<< HEAD
 };
 
 #endif
-=======
 
-
-};
->>>>>>> origin/master
