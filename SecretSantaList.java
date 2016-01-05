b8d5dc7faf3fd70a008afde83ad1f44e870561fc
Tristan Starck <ttstarck@gmail.com>
2016-01-05 12:32:09 -0800

    Added SecretSantaList.java

---
+++ SecretSantaList.java
@@ -0,0 +1,17 @@
+public class SecretSantaList{
+	private int listSize;
+	private SecretSanta[] santasList;
+
+	SecretSantaList(int size){
+		
+	}
+	void addSecretSanta(SecretSanta person){
+
+
+	}
+	void deleteSecretSanta(SecretSanta person){
+
+		
+	}
+
+}
