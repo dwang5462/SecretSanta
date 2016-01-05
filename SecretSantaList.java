f59ac515005ec5bb599f9ac37b7878b871531968
Tristan Starck <ttstarck@gmail.com>
2016-01-05 12:49:06 -0800

    Finished methods in SSList. Not tested yet

--- SecretSantaList.java
+++ SecretSantaList.java
@@ -1,17 +1,36 @@
 public class SecretSantaList{
 	private int listSize;
+	private int numSantasInList;
 	private SecretSanta[] santasList;
 
 	SecretSantaList(int size){
-		
+		listSize = size;
+		numSantasInList = 0;
+		santasList = new SecretSanta[listSize];
 	}
 	void addSecretSanta(SecretSanta person){
-
+		if (numSantasInList < listSize){
+			santasList[numSantasInList] = person;
+			numSantasInList++;
+		}
 
 	}
 	void deleteSecretSanta(SecretSanta person){
-
-		
+		int position = 0;
+		boolean deleteSanta = false;
+		for(; position < numSantasInList; position++){
+			if(santasList[position] == person;)
+				deleteSanta = true;
+				break;
+		}
+		for(int i = position+1; i < numSantasInList; i++){
+			santasList[i] = santasList[i-1];
+		}
+		if(deleteSanta == true){
+			santasList[numSantasInList] = null;
+			numSantasInList--;
+		}
 	}
+
 
 }
