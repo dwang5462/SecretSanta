718d43585047e93f6a442b5332278afc9d2d44f2
Tristan Starck <ttstarck@gmail.com>
2016-01-05 18:03:56 -0800

    RandomizeOrder and setAll recipients now implemented and tested

--- SecretSantaList.java
+++ SecretSantaList.java
@@ -19,17 +19,40 @@ public class SecretSantaList{
 		int position = 0;
 		boolean deleteSanta = false;
 		for(; position < numSantasInList; position++){
-			if(santasList[position] == person;)
+			if(santasList[position] == person)
 				deleteSanta = true;
 				break;
 		}
-		for(int i = position+1; i < numSantasInList; i++){
-			santasList[i] = santasList[i-1];
-		}
 		if(deleteSanta == true){
-			santasList[numSantasInList] = null;
+			for(int i = position+1; i < numSantasInList; i++){
+				santasList[i-1] = santasList[i];
+			}
+			santasList[numSantasInList-1] = null;
 			numSantasInList--;
+		}	
+	}
+	SecretSanta[] getSantasList(){
+		return santasList;
+	}
+	int getNumSantasInList(){
+		return numSantasInList;
+	}
+	void randomizeOrder(){
+		for(int i = 0; i<numSantasInList; i++){
+			int randomPos = (int)(Math.random()*numSantasInList);
+			SecretSanta tempSanta = santasList[i];
+			santasList[i] = santasList[randomPos];
+			santasList[randomPos] = tempSanta;
 		}
+
+	}
+	void setAllRecipients(){
+		for(int i = 0; i < numSantasInList-1; i++){
+			santasList[i].setRecipient(santasList[i+1]);
+		}
+		santasList[numSantasInList-1].setRecipient(santasList[0]);
+
+
 	}
 
 
